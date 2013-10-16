65102ef331dacde2a42a5afe660f3b922d5afe98
Ferdinand Majerech <kiithsacmp@gmail.com>
2013-10-16 10:44:49 +0200

    A wrapper exectuting a Process on relevant entities.

---
+++ entity/processwrapper.d
@@ -0,0 +1,65 @@
+//          Copyright Ferdinand Majerech 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// A wrapper that handles execution of a Process.
+module tharsis.entity.processwrapper;
+
+
+import std.algorithm;
+
+import tharsis.entity.entitymanager;
+
+// TODO: Allow processes to specify the maximum number of skipped updates 
+//       (default 0), as well as CPU overhead hints 
+//       (using an enum (trivial, cheap, medium, expensive, bottleneck))
+
+/// Abstract parent class to allow storing all process wrappers in a single array.
+class AbstractProcessWrapper
+{
+    /// Runs the process on all matching entities from specified entity manager.
+    void run(EntityManager entities) 
+    {
+        assert(false, "DMD bug workaround - should never happen");
+    }
+}
+
+/// Wraps a process of a concrete type.
+class ProcessWrapper(Process) : AbstractProcessWrapper
+{
+private:
+    /// The wrapped process.
+    Process process_;
+
+    /// A generated function that runs the process on all matching entities.
+    ProcessFunction runProcess_;
+
+public:
+    /// A generated function that runs the process on all matching entities.
+    ///
+    /// The EntityManager parameter provides the entities to process; the 
+    /// Process parameter is the process whose process() method/s will be 
+    /// called.
+    ///
+    /// Can be changed into delegate if needed, but try to keep it a function.
+    alias void function(EntityManager, Process) ProcessFunction;
+
+    /// Construct a ProcessWrapper.
+    /// 
+    /// Params: process    = The process to wrap.
+    ///         runProcess = Function that, passed the an entity manager and the
+    ///                      process, will run the process on all entities with
+    ///                      components matching the process() method/s of the 
+    ///                      process.
+    this(Process process, ProcessFunction runProcess)
+    {
+        process_    = process;
+        runProcess_ = runProcess;
+    }
+
+    override void run(EntityManager entities)
+    {
+        runProcess_(entities, process_);
+    }
+}
