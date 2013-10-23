004221a68202f020005e7690253f57a0f521ecd9
Ferdinand Majerech <kiithsacmp@gmail.com>
2013-10-23 10:46:19 +0200

    A dummy process template that preserves a component into future.

---
+++ defaults/copyprocess.d
@@ -0,0 +1,28 @@
+//          Copyright Ferdinand Majerech 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+/// A dummy process template that preserves a component into future.
+module tharsis.defaults.copyprocess;
+
+
+
+/// A dummy process template that preserves a component into future.
+///
+/// All this process does is that it copies the component of specified type into
+/// future space, ensuring the component does not disappear.
+class CopyProcess(ComponentType)
+{
+public:
+    /// OutComponent of this process is the copied component type.
+    alias ComponentType OutComponent;
+
+    /// Takes a past components and copies it to a future version.
+    void process(ref immutable(ComponentType) past, out ComponentType future)
+    {
+        future = past;
+    }
+}
+
