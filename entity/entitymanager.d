f7463bd9de004fac7a8013a292136c9529142ce3
Ferdinand Majerech <kiithsacmp@gmail.com>
2013-10-30 13:22:19 +0100

    Imports for EntityManager.

--- entity/entitymanager.d
+++ entity/entitymanager.d
@@ -6,3 +6,27 @@
 module tharsis.entity.entitymanager;
 
 
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+import std.traits;
+import std.typetuple;
+import std.typecons;
+import core.sync.mutex;
+
+import tharsis.entity.componentbuffer;
+import tharsis.entity.componenttypeinfo;
+import tharsis.entity.componenttypemanager;
+import tharsis.entity.defaultentitypolicy;
+import tharsis.entity.entity;
+import tharsis.entity.entityid;
+import tharsis.entity.entityprototype;
+import tharsis.entity.lifecomponent;
+import tharsis.entity.processtypeinfo;
+import tharsis.entity.processwrapper;
+import tharsis.entity.resourcemanager;
+import tharsis.util.bitmanip;
+import tharsis.util.mallocarray;
+
