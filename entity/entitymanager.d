aa1a80f7715ad7552466b08110f36322d5e7934b
Kiith-Sa <kiith-sa@solydxk>
2013-11-06 22:26:10 +0100

    Component and component count buffer for a single component type.

--- entity/entitymanager.d
+++ entity/entitymanager.d
@@ -48,5 +48,51 @@ class EntityManager(Policy)
     mixin validateEntityPolicy!Policy;
 
     alias Policy.ComponentCount ComponentCount;
+    /// All state belonging to one component type.
+    ///
+    /// Stores the components and component counts for each entity.
+    ///
+    /// Future versions of both the components and component counts are cleared 
+    /// when the frame begins, then added over the course of a frame.
+    struct ComponentTypeState
+    {
+    private:
+        /// True if this ComponentTypeState is used by an existing component 
+        /// type.
+        bool enabled_;
+
+    public:
+        /// Stores components as raw bytes.
+        ComponentBuffer!Policy buffer;
+
+        /// Stores component counts for every entity (at indices matching 
+        /// indices of entities in entity storage).
+        ComponentCountBuffer!Policy counts;
+
+        /// Enable the ComponentTypeState.
+        ///
+        /// Called when an existing component type will use this 
+        /// ComponentTypeState.
+        ///
+        /// Params: typeInfo = Type information about the type of components 
+        ///                    stored in this ComponentTypeState.
+        void enable(ref const(ComponentTypeInfo) typeInfo) @safe pure nothrow
+        {
+            buffer.enable(typeInfo.id, typeInfo.size);
+            counts.enable();
+            enabled_ = true;
+        }
+
+        /// Is there a component type using this ComponentTypeState?
+        bool enabled() @safe pure nothrow const { return enabled_; }
+
+        /// Reset the buffers, clearing them.
+        void reset() @safe pure nothrow
+        {
+            buffer.reset();
+            counts.reset();
+        }
+    }
+
 }
 
