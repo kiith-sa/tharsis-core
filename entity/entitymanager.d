2b3c1cd77106d7c7613ec09f08931533ef53f0ae
Kiith-Sa <kiith-sa@solydxk>
2013-11-24 16:42:19 +0100

    A basic EntityManager unittest.

--- entity/entitymanager.d
+++ entity/entitymanager.d
@@ -1094,3 +1094,72 @@ private:
     /// END CODE CALLED BY execute_frame() ///
     //////////////////////////////////////////
 }
+
+unittest 
+{
+    /// Not a 'real' Source, just for testing.
+    struct TestSource 
+    {
+    public:
+        struct Loader
+        {
+            TestSource loadSource(string name) @safe nothrow 
+            {
+                assert(false);
+            }
+        }
+
+        bool isNull() @safe nothrow const
+        {
+            assert(false);
+        }
+
+        bool readTo(T)(out T target) @safe nothrow
+        {
+            assert(false);
+        }
+
+        bool getSequenceValue(size_t index, out TestSource target) @safe nothrow
+        {
+            assert(false);
+        }
+
+        bool getMappingValue(string key, out TestSource target) @safe nothrow 
+        {
+            assert(false);
+        }
+    }
+
+    struct TimeoutComponent
+    {
+        enum ushort ComponentTypeID = maxBuiltinComponentTypes + 1;
+
+        enum minPrealloc = 8192;
+
+        int killEntityIn;
+    }
+
+    struct PhysicsComponent
+    {
+        enum ushort ComponentTypeID = maxBuiltinComponentTypes + 2;
+
+        enum minPrealloc = 16384;
+
+        enum minPreallocPerEntity = 1.0;
+
+        float x;
+        float y;
+        float z;
+    }
+
+
+    import tharsis.defaults.copyprocess;
+    auto compTypeMgr = 
+        new ComponentTypeManager!TestSource(TestSource.Loader());
+    compTypeMgr.registerComponentTypes!TimeoutComponent();
+    compTypeMgr.registerComponentTypes!PhysicsComponent();
+    compTypeMgr.lock();
+    auto entityManager = new EntityManager!DefaultEntityPolicy(compTypeMgr);
+    auto process = new CopyProcess!TimeoutComponent();
+    entityManager.registerProcess(process);
+}
