8622f44fe6546563b1578b5665fe972af9cd78eb
Kiith-Sa <kiith-sa@solydxk>
2013-11-29 18:54:01 +0100

    Added a module for user code to import to use builtin resource descriptors.

---
+++ defaults/descriptors.d
@@ -0,0 +1,10 @@
+//          Copyright Ferdinand Majerech 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+/// Resource descriptors built into Tharsis.
+module tharsis.defaults.descriptors;
+
+public import tharsis.entity.descriptors;
