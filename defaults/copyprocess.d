9bb7d6b71c2b56c13d13051e9716e2a04b09feb6
Kiith-Sa <kiith-sa@solydxk>
2013-12-07 17:25:08 +0100

    A CopyProcess for MultiComponents.

--- defaults/copyprocess.d
+++ defaults/copyprocess.d
@@ -31,3 +31,24 @@ public:
     }
 }
 
+/// A dummy process template that preserves components of a multi component type
+/// into future.
+///
+/// All this process does is that it copies the components of specified type 
+/// into future state, ensuring the components do not disappear.
+class CopyProcess(ComponentType)
+    if(isMultiComponent!ComponentType)
+{
+public:
+    /// FutureComponent of this process is the copied component type.
+    alias ComponentType FutureComponent;
+
+    /// Takes a past components and copies it to a future version.
+    void process(immutable ComponentType[] past,
+                 ref ComponentType[] future)
+    {
+        future   = future[0 .. past.length];
+        future[] = past[];
+    }
+}
+
