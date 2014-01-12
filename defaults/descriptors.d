2624eef22120608cc5d2b633a3d1a49598fd67ca
Kiith-Sa <kiith-sa@solydxk>
2014-01-12 01:42:47 +0100

    Added a resource descriptor wrapping a Source, to allow resources defined inline in e.g. YAML.

--- defaults/descriptors.d
+++ defaults/descriptors.d
@@ -8,3 +8,70 @@
 module tharsis.defaults.descriptors;
 
 public import tharsis.entity.descriptors;
+
+
+/// A resource descriptor storing a Source (such as YAML).
+///
+/// Used when a resource is defined inline in a source file
+/// (e.g. the 'override' section of a spawner component).
+///
+/// Params: Resource = The resource type the descriptor describes. Templating by 
+///                    the resource type avoids accidental assignments between
+///                    descriptors of different resource types.
+struct SourceWrapperDescriptor(Resource)
+{
+private:
+    /// Common parent class for source wrappers.
+    class AbstractSourceWrapper {}
+
+    /// Wraps a Source of a concrete type and provides access to it.
+    ///
+    /// Necessary to support any possible Source type without templating
+    /// the descriptor itself.
+    class SourceWrapper(Source): AbstractSourceWrapper
+    {
+        /// Construct a SourceWrapper wrapping a source of type Source.
+        this(ref Source source) @safe nothrow { this.source = source; }
+
+        /// The wrapped source.
+        Source source;
+    }
+
+    /// The wrapped source. Yes, this does use the GC (or rather, new).
+    ///
+    /// It's unlikely to cause significant overhead, however, since resources 
+    /// are shared. We may wrap Sources without using the GC in future,
+    /// but this may require tricky memory allocation.
+    AbstractSourceWrapper wrappedSource;
+
+public:
+    /// Access the wrapped source.
+    ///
+    /// This should be used to initialize the resource described by this
+    /// descriptor.
+    @property ref Source source(Source)() @trusted pure nothrow 
+    {
+        auto wrapper = cast(SourceWrapper!Source)wrappedSource;
+        assert(wrapper !is null, 
+               "Trying to unwrap a source in a SourceWrapperDescriptor as a "
+               "different source type (e.g. YAML as XML)");
+        return wrapper.source;
+    }
+
+    /// Load a SourceWrapperDescriptor from a Source such as YAML.
+    ///
+    /// Used by Tharsis to initialize the descriptor.
+    /// 
+    /// Params:  source = Source to load from.
+    ///          result = The descriptor will be written here, if loaded 
+    ///                   succesfully.
+    ///
+    /// Returns: true if succesfully loaded, false otherwise.
+    static bool load(Source)(ref Source source, 
+                             out SourceWrapperDescriptor result) 
+        @safe nothrow
+    {
+        result.wrappedSource = new SourceWrapper!Source(source);
+        return true;
+    }
+}
