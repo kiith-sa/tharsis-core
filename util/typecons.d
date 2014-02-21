0b01c31a1498680839d55d93d51587d9bf6d7ab3
Kiith-Sa <kiith-sa@solydxk>
2014-02-21 11:10:20 +0100

    A simple wrapper turning a struct into a class.

---
+++ util/typecons.d
@@ -0,0 +1,21 @@
+//          Copyright Ferdinand Majerech 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// An extension to std.typecons.
+module tharsis.util.typecons;
+
+public import std.typecons;
+
+/// A simple wrapper to turn a struct type into a class type.
+class Class(Struct)
+{
+    /// The wrapped struct.
+    Struct self_;
+
+    /// Make the class behave as if it was Struct.
+    alias self_ this;
+}
+
+
