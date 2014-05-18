b82cf717112a22132cc47c13bc18f9eb427d5f1c
Kiith-Sa <kiith-sa@solydxk>
2014-05-18 17:37:07 +0200

    A template function to validate the signature of a method.

--- util/traits.d
+++ util/traits.d
@@ -83,3 +83,142 @@ template returnsByRef(alias func)
 {
     enum returnsByRef = (functionAttributes!func & FunctionAttribute.ref_) != 0;
 }
+/// Call this to force the caller to be impure
+/// (to block automatic inference of purity for functions passed to templates).
+void unPurifier() @trusted nothrow
+{
+    static __gshared uint uglyGlobal;
+    uglyGlobal += 6;
+}
+
+/// Validate signature of a method (member function) by comparing to a reference method.
+///
+/// Params:  T           = The type (class or struct) that has the method.
+///          method      = Name of the method.
+///          expectedAPI = A method with the signature we expect the validated method to 
+///                        have.
+///
+/// To use validateMethodAPI declare a dummy method with expected signature and call
+/// validateMethodAPI with the type/method we're validating and the dummy method.
+/// This is usually useful when validating complex concepts such as Source or Process
+/// which use compile-time polymorphism instead of interfaces and virtual methods.
+/// 
+/// validateMethodAPI checks that the method has the same return type and parameters
+/// as the reference method. It also checks the storage classes of the parameters and
+/// attributes of the method. These may not be exactly the same in the validated method
+/// as in the reference method, but they must be "good enough or better".
+///
+/// The rules for parameter storage classes are as follows:
+///
+/// * If a parameter in ExpectedAPI is lazy, the parameter must be lazy.
+/// * If a parameter in ExpectedAPI is not lazy, the parameter must not be lazy.
+/// * If a parameter in ExpectedAPI is scope, the parameter must be scope.
+/// * If a parameter in ExpectedAPI is not ref, the parameter must not be ref.
+/// * If a parameter in ExpectedAPI is not out, the parameter must not be out.
+///
+/// The rules for function attributes:
+///
+/// * If ExpectedAPI is @safe/@trusted, the method must be @safe/@trusted.
+/// * If ExpectedAPI is nothrow, the method must be nothrow.
+/// * If ExpectedAPI is pure, the method must be pure.
+/// * If ExpectedAPI returns by ref, the method must return by ref.
+/// * If ExpectedAPI does not return by ref, the method must not return by ref.
+///
+/// Template methods:
+///
+/// At this moment, validateMethodAPI doesn't handle template methods. To validate at 
+/// least the non-template aspects of a template method, use a concrete instantiation
+/// as the $(D method) parameter, e.g. 
+/// validateMethodAPI(MyStruct, "doStuff!int", doStuffReference);
+///
+/// Purity:
+///
+/// The compiler may automatically determine purity of a method passed as ExpectedAPI.
+/// To ensure that the dummy method passed as ExpectedAPI is not pure when purity is not
+/// required for the validated methods, call the tharsis.util.traits.unPurifier()
+/// function from the dummy method.
+string validateMethodAPI(T, string method, alias ExpectedAPI)()
+{
+    // TODO handle template methods separately and remove the findSplit hack below.
+    const type = T.stringof;
+    // If the method is an instantation of a template method the member will be without
+    // the template parameter.
+    if(!__traits(hasMember, T, method.findSplit("!")[0]))
+    {
+        return "Type %s does not have expected method '%s'\n".format(type, method);
+    }
+
+    mixin(q{
+    alias Method = T.%s;
+    }.format(method));
+
+    alias ExpectedRet            = ReturnType!ExpectedAPI;
+    alias ExpectedParams         = ParameterTypeTuple!ExpectedAPI;
+    alias StorageClasses         = ParameterStorageClassTuple!Method;
+    alias ExpectedStorageClasses = ParameterStorageClassTuple!ExpectedAPI;
+
+    string[] lines;
+    static if(!is(ReturnType!Method == ExpectedRet))
+    {
+        lines ~= "must return an instance of %s".format(ExpectedRet.stringof);
+    }
+    static if(!is(ParameterTypeTuple!Method == ExpectedParams))
+    {
+        lines ~= "must have the following parameters: " ~ ExpectedParams.stringof;
+    }
+    else foreach(i, expected; ExpectedStorageClasses)
+    {
+        alias ParameterStorageClass STC;
+        auto storageClass = StorageClasses[i];
+        if(storageClass == expected) { continue; }
+
+        // Lazy might evaluate when not expected so we strictly want the API to match.
+        auto isLazy = expected & STC.lazy_;
+        // If we expect scope, the method must use scope as otherwise it may escape 
+        // references.
+        auto isScope = expected & STC.scope_;
+        // If we expect out or ref, it's OK to not use them; the method might not want
+        // to overwrite the caller's values. But if we _don't_ expect them, we must
+        // enforce they are not used as the method might unexpectedly modify the 
+        // caller's values.
+        auto isOut  = expected & STC.out_;
+        auto isRef  = expected & STC.ref_;
+
+        if((storageClass & STC.lazy_) != isLazy)
+        {
+            lines ~= "parameter %s must%s be lazy".format(i, isLazy ? "" : " not");
+        }
+        if(!(storageClass & STC.scope_) && isScope)
+        {
+            lines ~= "parameter %s must be scope";
+        }
+        if((storageClass & STC.out_) && !isOut)
+        {
+            lines ~= "parameter %s must not be out";
+        }
+        if((storageClass & STC.ref_) && !isRef)
+        {
+            lines ~= "parameter %s must not be ref";
+        }
+    }
+
+    static if(!isSafe!Method && isSafe!ExpectedAPI)
+    {
+        lines ~= "must be @safe or @trusted";
+    }
+    static if(!isNothrow!Method && isNothrow!ExpectedAPI)
+    {
+        lines ~= "must be nothrow";
+    }
+    static if(!isPure!Method && isPure!ExpectedAPI)
+    {
+        lines ~= "must be pure";
+    }
+    static if(returnsByRef!Method != returnsByRef!ExpectedAPI)
+    {
+        lines ~= "must%s return by ref".format(returnsByRef!ExpectedAPI ? "" : " not");
+    }
+
+    string base = "Invalid API: member %s of type %s: ".format(method, type);
+    return lines.empty ? null : base ~ "\n" ~ lines.join("\n") ~ "\n\n";
+}
