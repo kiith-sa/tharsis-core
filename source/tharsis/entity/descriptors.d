6c614253ee9e7dd31469d6120160dab91dd5130e
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-08-14 22:17:24 +0200

    Stripped spaces.

--- source/tharsis/entity/descriptors.d
+++ source/tharsis/entity/descriptors.d
@@ -5,7 +5,7 @@
 
 /// Resource descriptors used by builtin resources.
 ///
-/// These descriptors can also be used by user code, but it is recommended to 
+/// These descriptors can also be used by user code, but it is recommended to
 /// import tharsis.defaults.descriptors instead of this module.
 module tharsis.entity.descriptors;
 
@@ -13,7 +13,7 @@ module tharsis.entity.descriptors;
 ///
 /// Used e.g. for resources loaded from files with file name as the descriptor.
 ///
-/// Params: Resource = The resource type the descriptor describes. Templating by 
+/// Params: Resource = The resource type the descriptor describes. Templating by
 ///                    the resource type avoids accidental assignments between
 ///                    descriptors of different resource types.
 struct StringDescriptor(Resource)
@@ -22,13 +22,13 @@ struct StringDescriptor(Resource)
     string fileName;
 
     /// Load a StringDescriptor from a Source such as YAML.
-    /// 
+    ///
     /// Params:  source = Source to load from.
-    ///          result = The descriptor will be written here, if loaded 
+    ///          result = The descriptor will be written here, if loaded
     ///                   succesfully.
     ///
     /// Returns: true if succesfully loaded, false otherwise.
-    static bool load(Source)(ref Source source, out StringDescriptor result) 
+    static bool load(Source)(ref Source source, out StringDescriptor result)
         @safe nothrow
     {
         return source.readTo(result.fileName);
@@ -39,7 +39,7 @@ struct StringDescriptor(Resource)
     ///
     /// Usually, this returns true if two descriptors describe the same resource
     /// (e.g. if the descriptors are equal).
-    /// 
+    ///
     /// The resource manager uses this when a resource handle is requested to
     /// decide whether to load a new resource or to reuse an existing one
     /// (if a descriptor maps to the same handle as a descriptor of already
