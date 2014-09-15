d87ccf25804817f89043386c622dec81732e87ad
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-09-15 06:01:26 +0200

    GameState cleanup.

--- source/tharsis/entity/gamestate.d
+++ source/tharsis/entity/gamestate.d
@@ -18,12 +18,13 @@ import tharsis.util.noncopyable;
 
 package:
 
-/// All state belonging to one component type.
-///
-/// Stores the components and component counts for each entity.
-///
-/// Future versions of both the components and component counts are cleared when the
-/// frame begins, then added over the course of a frame.
+/** All state belonging to one component type.
+ *
+ * Stores the components and component counts for each entity.
+ *
+ * Future versions of both the components and component counts are cleared when the frame
+ * begins, then added over the course of a frame.
+ */
 struct ComponentTypeState(Policy)
 {
     mixin(NonCopyable);
@@ -33,18 +34,19 @@ public:
     /// Stores components as raw bytes.
     ComponentBuffer!Policy buffer;
 
-    /// Component counts for every entity (counts[i] is the number of components for
-    /// entity at entities[i] in entity storage).
+    /** Component counts for every entity (counts[i] is the number of components for the
+     * entity at entities[i] in entity storage).
+     */
     MallocArray!ComponentCount counts;
 
-    /** Offsets of the first component for every entity (offsets[i] is the index of
-     * the first component for entity at entities[i] in entity storage).
+    /** Offsets of the first component for every entity (offsets[i] is the index of the
+     * first component for entity at entities[i] in entity storage).
      *
-     * For entities that have zero components of this type, the offset is uint.max
-     * (absurd value to detect bugs).
+     * For entities that have zero components of this type, the offset is uint.max (absurd
+     * value to detect bugs).
      *
-     * Allows fast access to components of any past entity (if we know its index);
-     * this enables direct component access through EntityAccess.
+     * Allows fast access to components of any past entity (if we know its index); this
+     * enables direct component access through EntityAccess.
      */
     MallocArray!uint offsets;
 
@@ -62,32 +64,34 @@ public:
         if(enabled_) { reset(); }
     }
 
-    /// Enable the ComponentTypeState.
-    ///
-    /// Called when an existing component type will use this ComponentTypeState.
-    ///
-    /// Params: typeInfo = Type information about the type of components stored in
-    ///                    this ComponentTypeState.
+    /** Enable the ComponentTypeState.
+     *
+     * Called when an existing component type will use this ComponentTypeState.
+     *
+     * Params: typeInfo = Type information about the type of components stored in this
+     *                    ComponentTypeState.
+     */
     void enable(ref const(ComponentTypeInfo) typeInfo) @safe pure nothrow
     {
-        assert(!enabled_, "Trying to enable ComponentTypeState that's already "
-                          "enabled. Maybe 2 component types use the same type ID?");
+        assert(!enabled_, "Trying to enable ComponentTypeState that's already enabled. "
+                          " Maybe 2 component types use the same type ID?");
 
         buffer.enable(typeInfo.id, typeInfo.size);
         enabled_ = true;
     }
 
     /// Is there a component type using this ComponentTypeState?
-    bool enabled() @safe pure nothrow const { return enabled_; }
+    bool enabled() @safe pure nothrow const @nogc { return enabled_; }
 
-    /// Grow the number of entities to store component counts for.
-    ///
-    /// Can only be used to _increase_ the number of entities. Component counts for
-    /// the new entities are set to 0. Used by EntityManager e.g. after determining
-    /// the number of future entities and before adding newly created entities.
-    ///
-    /// Params: count = New entity count. Must be greater than the current entity
-    ///                 count (set by a previous call to reset or growEntityCount).
+    /** Grow the number of entities to store component counts for.
+     *
+     * Can only be used to _increase_ the number of entities. Component counts for the new
+     * entities are set to 0. Used by EntityManager e.g. after determining the number of
+     * future entities and before adding newly created entities.
+     *
+     * Params: count = New entity count. Must be greater than the current entity count
+     *                 (set by a previous call to reset or growEntityCount).
+     */
     void growEntityCount(const size_t count)
     {
         assert(enabled_, "This ComponentTypeState is not enabled");
@@ -100,9 +104,10 @@ public:
         offsets[oldSize .. $] = uint.max;
     }
 
-    /// Reset the buffers, clearing them.
-    ///
-    /// Sets the entity count to 0.
+    /** Reset the buffers, clearing them.
+     *
+     * Sets the entity count to 0.
+     */
     void reset() @safe pure nothrow
     {
         assert(enabled_, "This ComponentTypeState is not enabled");
@@ -112,15 +117,17 @@ public:
     }
 }
 
-/// Stores components of all entities (either past or future).
-///
-/// Also stores component counts of every component type for every entity.
+/** Stores components of all entities (either past or future).
+ *
+ * Also stores component counts of every component type for every entity.
+ */
 struct ComponentState(Policy)
 {
     mixin(NonCopyable);
 public:
-    /// Stores component/component count buffers for all component types at
-    /// indices set by the ComponentTypeID members of the component types.
+    /** Stores component/component count buffers for all component types at indices set by
+     * the ComponentTypeID members of the component types.
+     */
     ComponentTypeState!Policy[maxComponentTypes!Policy] self_;
 
     /// Access the component type state array directly.
@@ -128,17 +135,19 @@ public:
 
     @disable this();
 
-    /// Clear the buffers.
-    ///
-    /// Used to clear future component buffers when starting a frame.
+    /** Clear the buffers.
+     *
+     * Used to clear future component buffers when starting a frame.
+     */
     void resetBuffers()
     {
         foreach(ref data; this) if(data.enabled) { data.reset(); }
     }
 
-    /// Inform the component counts buffers about increased (or equal) entity count.
-    ///
-    /// Called between frames when entities are added.
+    /** Inform the component counts buffers about increased (or equal) entity count.
+     *
+     * Called between frames when entities are added.
+     */
     void growEntityCount(const size_t count)
     {
         foreach(ref data; this) if(data.enabled)
@@ -148,9 +157,10 @@ public:
     }
 }
 
-/// Stores all game state (entities and components).
-///
-/// EntityManager has two instances of GameState; past and future.
+/** Stores all game state (entities and components).
+ *
+ * EntityManager has two instances of GameState; past and future.
+ */
 struct GameState(Policy)
 {
     /* TODO: An alternative implementation of GameState storage to try:
@@ -158,38 +168,34 @@ struct GameState(Policy)
      * For every component type, there is a buffer of entityID-componentIdx pairs.
      *
      * A pair says 'this entity has this component'. If an entity has more than 1
-     * component, simply have multiple pairs. (Or, for MultiComponents, use triplets
-     * instead of pairs, with the last member of the triplet being component count)
+     * component, have multiple pairs. (Or, MultiComponents, use triplets instead of
+     * pairs, with the last member of the triplet being component count)
      *
      * When running a process, we run only through buffers of the relevant component
-     * types. Meaning we don't run over _all_ entities. Especially relevant for
-     * systems processing unusual components.
+     * types. Meaning we don't run over _all_ entities. Especially relevant for systems
+     * processing unusual components.
      *
-     *
-     * We could implement this and compare speed. Perhaps even have 2 switchable
-     * implementations (within the entity manager) - depending on performance either
-     * a class member with derived implementations or a struct template parameter.
+     * Implement this and compare speed. Perhaps even have 2 switchable implementations.
      */
     /// Stores components of all entities.
     ComponentState!Policy components;
 
     //TODO entities should be manually allocated.
-    /// All existing entities (either past or future).
-    ///
-    /// Ordered by entity ID. This is necessary to enable direct component
-    /// access through EntityAccess.
-    ///
-    /// The entire length of this array is used; it doesn't have a unused
-    /// part at the end.
+    /**
+     * All existing entities (either past or future).
+     *
+     * Ordered by entity ID. This is necessary to enable direct component access through
+     * EntityAccess.
+     *
+     * The entire length of this array is used; it doesn't have a unused part at the end.
+     */
     Entity[] entities;
 
     /* TODO: May add a structure to access entities by entityID to speed up direct
-     * component access with EntityAccess (currently using binary search). Could use
-     * some hash map, or a multi-level bucket-sorted structure (?) (E.g. 65536 buckets
-     * for the first 16 bits of the entity ID, and arrays/slices in the buckets)
+     * component access with EntityAccess (currently using binary search). Could use some
+     * hash map, or a multi-level bucket-sorted structure (?) (E.g. 65536 buckets for the
+     * first 16 bits of the entity ID, and arrays/slices in the buckets)
      *
      * Would have to be updated with the entity array between frames.
      */
 }
-
-
